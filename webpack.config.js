f10bf4fd9e2651e8a30e5f547bdc90a0e3bdf244
scottandrewlepera <scott@scottandrew.com>
2019-11-14 22:35:25 -0800

    configure webpack to use local typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './build/index.js',
+  entry: './src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: "none"
 };
